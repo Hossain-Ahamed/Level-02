dc74411189d394cbc6f226fa70d52e21bc8cab5e
Hossain Ahamed <hossainahamed6872@gmail.com>
2024-06-03 22:09:17 +0600

    point

---
+++ M-2 Mongo/Mongoose/m-8-first-project/src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors'
+const app = express()
+const port = 3000
+
+//parser
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+export default app;
---
+++ M-2 Mongo/Mongoose/m-8-first-project/src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import config from "./app/config";
+import app from "./app";
+
+
+async function main() {
+    try{
+        await mongoose.connect(config.database_URL as string);
+
+        app.listen(config.PORT, () => {
+          console.log(`Example app listening on port ${config.PORT}`);
+        });
+    }catch(error){
+        console.log(error);
+        
+    }
+ 
+}

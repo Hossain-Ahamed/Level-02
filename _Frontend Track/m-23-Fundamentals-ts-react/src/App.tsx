1ba3ea2d08e5687a8fb668c90064041769ad8cb4
Hossain-Ahamed <hossainahamed6872@gmail.com>
2024-11-07 21:14:07 +0600

    update

--- _Frontend Track/m-23-Fundamentals-ts-react/src/App.tsx
+++ _Frontend Track/m-23-Fundamentals-ts-react/src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import './App.css'
 import UserStateExample from './pages/UserStateExample'
+import Form from './pages/Form';
 
 function App() {
   const [counter, setCounter] = useState(0);
 
   return (
     <>
-     <UserStateExample counter={counter} setCounter={setCounter}/>
+     {/* <UserStateExample counter={counter} setCounter={setCounter}/> */}
+     <Form/>
     </>
   )
 }
--- _Frontend Track/m-23-Fundamentals-ts-react/src/pages/Form.tsx
+++ _Frontend Track/m-23-Fundamentals-ts-react/src/pages/Form.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  //   const [name, setName] = useState("");
+  //   const [email, setEmail] = useState("");
+  const [user, setUser] = useState({ name: "", email: "" });
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
+  const handelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputName = e.target.name;
+    const value = e.target.value;
+    setUser({ ...user, [inputName]: value });
+  };
+  console.log(user);
   return (
-    <form>
-      <input type="text" name="name" id="name" />
-      <input type="text" name="email" id="email" />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        className="border p-2 m-2 rounded"
+        onChange={handelChange}
+      />
+      <input
+        type="text"
+        name="email"
+        id="email"
+        className="border p-2 m-2 rounded"
+        onChange={handelChange}
+      />
       <button type="submit">Submit</button>
     </form>
   );

a79060bef9604d3cad89488235791bd7955fd409
Hossain-Ahamed <hossainahamed6872@gmail.com>
2024-11-13 01:12:48 +0600

    j

---
+++ _Frontend Track/react-reusable-component-module-4/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import MainLayout from './components/layout/MainLayout'
+
+function App() {
+
+  return (
+    <>
+    <h1>React App</h1>
+      <MainLayout/>
+    </>
+  )
+}
+
+export default App ;

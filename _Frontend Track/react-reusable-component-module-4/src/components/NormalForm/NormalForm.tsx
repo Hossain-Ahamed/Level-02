9d329429c0480833acdc04032bd80fd9a6242edc
Hossain-Ahamed <hossainahamed6872@gmail.com>
2025-01-04 18:23:16 +0600

    learning reusablity 1

--- _Frontend Track/react-reusable-component-module-4/src/components/NormalForm/NormalForm.tsx
+++ _Frontend Track/react-reusable-component-module-4/src/components/NormalForm/NormalForm.tsx
@@ -5,7 +5,7 @@ import { Button } from "../UI/Button";
 
 const NormalForm = () => {
   const double = true;
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit  ,formState : {errors}} = useForm();
   const onSubmit = (data) => {
     console.log(data);
   };
@@ -21,17 +21,17 @@ const NormalForm = () => {
         <div
           className={cn("grid gap-5 justify-items-center ", {
             "md:grid-cols-2": double,
-            "md:grid-cols-1": !double,
+           
           })}
         >
           <div className="w-full max-w-md">
             <label htmlFor="name" className="block ">
               Name
             </label>
-            <input type="text" {...register("name")} id="name" className="" />
+            <input type="text" {...register("name",{required:true})} id="name" className="" />
           </div>
           <div className="w-full max-w-md">
-            <label htmlFor="name" className="block ">
+            <label htmlFor="email" className="block ">
               Email
             </label>
             <input
@@ -41,30 +41,19 @@ const NormalForm = () => {
               className=""
             />
           </div>
+          <div className="w-full max-w-md">
+            <label htmlFor="password" className="block ">
+              Password
+            </label>
+            <input
+              type="password"
+              {...register("password")}
+              id="password"
+              className=""
+            />
+          </div>
 
-          <div className="w-full max-w-md">
-            <label htmlFor="name" className="block ">
-              select
-            </label>
-            <select name="" id="">
-              <option value="">1</option>
-              <option value="">2</option>
-              <option value="">3</option>
-              <option value="">4</option>
-            </select>
-          </div>
-          <div className="w-full max-w-md">
-            <label htmlFor="name" className="block ">
-              select
-            </label>
-            <textarea name="" id=""></textarea>
-          </div>
-          <div className="w-full max-w-md">
-            <label htmlFor="name" className="block ">
-              select
-            </label>
-            <input type="checkbox" name="" id="" className="" />
-          </div>
+       
         </div>
         <div
           className={cn("grid grid-cols-1 justify-items-center gap-5 my-8", {

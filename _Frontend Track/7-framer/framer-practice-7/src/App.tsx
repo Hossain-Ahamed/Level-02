8dee86a949a2c1a484b4ea6e71aab1c4c537b09b
Hossain-Ahamed <hossainahamed6872@gmail.com>
2025-01-12 20:45:12 +0600

    doc updated

--- _Frontend Track/7-framer/framer-practice-7/src/App.tsx
+++ _Frontend Track/7-framer/framer-practice-7/src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { motion } from "motion/react";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <motion.div className="size-64 bg-indigo-500 rounded-lg"
+      initial={{rotate:0}}
+      animate={{rotate : 360}}
+      transition={{ duration: 2, type : 'spring' }}
+      ></motion.div>
+    </div>
+  );
+};
 
-export default App
+export default App;

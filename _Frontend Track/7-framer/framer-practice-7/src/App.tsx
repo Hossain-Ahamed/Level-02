a5c8ab44a17e6ac7ecf855d022008718502be555
Hossain-Ahamed <hossainahamed6872@gmail.com>
2025-01-13 19:15:08 +0600

    motion framer

--- _Frontend Track/7-framer/framer-practice-7/src/App.tsx
+++ _Frontend Track/7-framer/framer-practice-7/src/App.tsx
@@ -1,14 +1,15 @@
 import { motion } from "motion/react";
 import React from "react";
 
+const initial = {rotate:0};
+const animate = {rotate:0};
 const App = () => {
   return (
     <div>
       <motion.div className="size-64 bg-indigo-500 rounded-lg"
-      initial={{rotate:0}}
-      animate={{rotate : 360}}
-      transition={{ duration: 2, type : 'spring' }}
-      ></motion.div>
+      initial={initial}
+      animate={animate}
+      transition={{duration:0,type:'spring'}}></motion.div>
     </div>
   );
 };

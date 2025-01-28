7f15fa8f59dd239c2cf54215e2e9822cdfc5a6ae
Hossain-Ahamed <hossainahamed6872@gmail.com>
2025-01-28 08:42:29 -0800

    add

--- _Frontend Track/7-framer/framer-practice-7/src/App.tsx
+++ _Frontend Track/7-framer/framer-practice-7/src/App.tsx
@@ -3,6 +3,7 @@ import Lesson2 from './Pages/Lesson2';
 import Lesson3 from './Pages/Lesson3';
 import Lesson4 from './Pages/Lesson4';
 import Lesson5 from './Pages/Lesson5.keyframe.tsx';
+import Lesson6 from './Pages/Lesson6.tsx';
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       {/* <Lesson2/> */}
       {/* <Lesson3/> */}
       {/* <Lesson4/> */}
-      <Lesson5/>
+      {/* <Lesson5/> */}
+      <Lesson6/>
 
     </>
   );
---
+++ _Frontend Track/7-framer/framer-practice-7/src/Pages/Lesson6.tsx
@@ -0,0 +1,9 @@
+import { motion,useAnimate,useAnimationControls } from "motion/react";
+
+
+const Lesson6 = () => {
+  
+    return <ul ref={scope}><li>fdaskfj</li></ul>
+};
+
+export default Lesson6;
